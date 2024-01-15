39e834fc7a0b16e30233911966668e2ebe0cad14
jungAh,Han <truth.h406@gmail.com>
2024-01-15 16:48:51 +0900

    begin the project

---
+++ header.js
@@ -0,0 +1,7 @@
+document.getElementById('logo').addEventListener('click', () => {
+  window.location.href = './index.html';
+});
+
+document.getElementById('cart-button').addEventListener('click', () => {
+  window.location.href = './cart.html';
+});
